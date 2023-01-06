41f0ef19321c3e8c27e06a1133c3e24d05b62504
hz <dxp@ngs.ru>
2023-01-06 13:33:56 +0700

    split adder implementation in BD and HLS variants

--- src/syn/adder_bd.sv
+++ src/syn/adder_bd.sv
@@ -4,66 +4,11 @@
 //
 //     Purpose: Adder module. Wraps block design
 //
-//
 //-------------------------------------------------------------------------------
 
 
-
-
-//-------------------------------------------------------------------------------
-interface dinp_if
-#(
-    parameter DATA_W = 4
-);
-    
-typedef logic [DATA_W-1:0] data_t;
-    
-data_t data;
-logic  valid;
-logic  ready;
-    
-modport m
-(
-    output data,
-    output valid,
-    input  ready
-);    
-    
-modport s
-(
-    input  data,
-    input  valid,
-    output ready
-);    
-endinterface
-//-------------------------------------------------------------------------------
-interface dout_if
-#(
-    parameter DATA_W = 4
-);
-
-typedef logic [DATA_W-1:0] data_t;
-
-data_t data;
-logic  valid;
-
-modport m
-(
-    output data,
-    output valid
-);    
-
-modport s
-(
-    input  data,
-    input  valid
-);    
-endinterface
 //-------------------------------------------------------------------------------
 module automatic adder_m
-#(
-    parameter DATA_W = 4
-)
 (
     input logic  clk,
     input logic  rst,
@@ -105,11 +50,6 @@ always_ff @(posedge clk) begin
     out.valid <= adder_en;  // add pipeline delay
 end
     
-always_comb begin
-    a.ready = !a_full;
-    b.ready = !b_full;
-end
-    
 //------------------------------------------------------------------------------
 //
 //    Instances
@@ -120,10 +60,10 @@ adder_wrapper adder_bd
     .rst            ( rst                ),
     .dinp_a_full    ( a_full             ),
     .dinp_a_wr_data ( a.data             ),
-    .dinp_a_wr_en   ( a.valid && a.ready ),
+    .dinp_a_wr_en   ( a.valid && !a_full ),
     .dinp_b_full    ( b_full             ),
     .dinp_b_wr_data ( b.data             ),
-    .dinp_b_wr_en   ( b.valid && b.ready ),
+    .dinp_b_wr_en   ( b.valid && !b_full ),
     .dout           ( out.data           ),
     .adder_en       ( adder_en           )
 
